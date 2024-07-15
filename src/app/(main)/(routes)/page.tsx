2776678e6619827492bacd71f00f4f0688de48cb
maybeajay <heyajayramola@gmail.com>
2024-07-15 22:50:34 +0530

    added schema and modal for prisma and initial setup for prisma

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from '@/components/ui/mode-toggle'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div>This is a protected route</div>
+    <ModeToggle />
+    </>
+  )
+}
+
+export default page
